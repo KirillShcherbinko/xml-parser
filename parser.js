99c9df94fc5556128e64ed5a032892cea4adfe6b
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2024-12-08 20:52:04 +0300

    updated function for parsing value

--- parser.js
+++ parser.js
@@ -1,33 +1,48 @@
 import { DOMParser } from "xmldom";
 
-export function parseValue(element, indent, constants) {
+export function parseValue(element, indent) {
     // Формируем массив из дочерних тегов
     const childElements = Array.from(element.childNodes)
         .filter(node => node.nodeType === 1);
 
     const attributes = element.attributes;
 
+    // Игнорируем первую строку файла
+    if (attributes.version) {
+        return;
+    }
+
     if (childElements.length === 0 && element.nodeType === 1 && attributes.name && attributes.value) {
         constants[attributes.name] = attributes.value;
         return `define ${attributes.name} ${attributes.value}`;
     }
 
-    // Проверка на константу (имеет целочисленное значение)
-    if (childElements.length === 0 && element.nodeType === 1) {
-        const text = element.textContent.trim();
+    // Работа с целосленными значениями
+    if (childElements.length === 0 || attributes.operation && element.nodeType === 1) {
+        let text;
+        if (!attributes.operation) {
+            text = element.textContent.trim();
+        }
+        console.log(attributes.operation)
         if (!isNaN(text)) {
             return text;
         } else if (constants[text]) {
-
             element.textContent = constants[text];
-            console.log(element);
-            return parseValue(element, indent, constants);
-        } 
+            return parseValue(element, indent);
+        } else if (attributes.operation === "+" && childElements.length === 2) {
+            return `$[+ ${parseValue(childElements[0], indent)} ${parseValue(childElements[1], indent)}]`
+        } else if (attributes.operation === "-" && childElements.length === 2) {
+            return `$[- ${parseValue(childElements[0], indent)} ${parseValue(childElements[1], indent)}]`
+        } else if (attributes.operation === "abs" && childElements.length === 1) {
+            return `$[abs ${parseValue(childElements[0], indent)}]`
+        } else if (attributes.operation === "min") {
+            return `$[min ${childElements.map(child => parseValue(child, indent)).join(" ")}]`
+        }
     }
 
     // Проверка на массив
     if (childElements.length > 0 && childElements.every(child => child.tagName === childElements[0].tagName)) {
-        return `list(${childElements.map(child => parseValue(child, indent, constants)).join(", ")})`;
+        return `list(${childElements.map(child => parseValue(child, indent)).join(", ")})`;
     }
 
     // Проверка на словарь
@@ -36,7 +51,7 @@ export function parseValue(element, indent, constants) {
 
         // Добавление элементов в словарь
         childElements.forEach(child => {
-            pairs.push(`    ${indent}${child.tagName} : ${parseValue(child, "    " + indent, constants)}`);
+            pairs.push(`    ${indent}${child.tagName} : ${parseValue(child, "    " + indent)}`);
         });
 
         return `{\n${pairs.join('\n')}\n${indent}}`;
@@ -94,9 +109,12 @@ const xmlString = `
     <Tite>2</Tite>
     <dict>
         <c id="56">1</c>
-        <d>
+        <d operation="+">
             <l>var</l>
-            <l>89</l>
+            <l operation="-">
+                <j>8</j>
+                <s>908</s>
+            </l>
         </d>
         <v>2</v>
     </dict>
@@ -111,7 +129,7 @@ const result = rootNodes
     .filter(item => item !== "");
 const constants = {};
 const resultString = result
-    .map(elem => parseValue(elem, "", constants)) // Преобразуем каждый объект в строку
+    .map(elem => parseValue(elem, "")) // Преобразуем каждый объект в строку
     .join('\n'); 
     
 console.log(resultString);
