466cf622338e25a87051bc1809da0f1b6d940b5d
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2024-12-09 02:53:44 +0300

    added functions for reading and writing files and main function

--- parser.js
+++ parser.js
@@ -1,6 +1,46 @@
+import { readFileSync, writeFileSync } from "fs";
 import { DOMParser } from "xmldom";
 
-export function parseValue(element, indent) {
+
+export function readKeys() {
+    const args = process.argv.slice(2);
+
+    // Проверка на число введённых аргументов
+    if (args.length > 2) {
+        throw new Error("Введено слишком много аргументов");
+    } else if (args.length < 2) {
+        throw new Error("Введено недостаточно аргументов");
+    }
+    
+    // Объект с ключами командной строки
+    const keys = {
+        xmlFilePath: args[0],
+        txtFilePath: args[1]
+    };
+
+    return keys;
+}
+
+
+export function readXmlFile(filePath) {
+    try {
+        const xmlString = readFileSync(filePath, 'utf-8');
+        return xmlString;
+    } catch (err) {
+        throw new Error(`Ошибка при чтении файла: ${err.message}`);
+    }
+}
+
+export function writeTxtFile(filePath, text) {
+    try {
+        writeFileSync(filePath, text, 'utf-8');
+        return `Файл ${filePath} успешно записан`;
+    } catch (err) {
+        throw new Error(`Ошибка при записи в файл: ${err.message}`);
+    }
+}
+
+export function parseValue(element, indent, constants) {
     // Формируем массив из дочерних тегов
     const childElements = Array.from(element.childNodes)
         .filter(node => node.nodeType === 1);
@@ -12,37 +52,37 @@ export function parseValue(element, indent) {
         return;
     }
 
+    // Проверка на константу
     if (childElements.length === 0 && element.nodeType === 1 && attributes.name && attributes.value) {
         constants[attributes.name] = attributes.value;
         return `define ${attributes.name} ${attributes.value}`;
     }
 
-    // Работа с целосленными значениями
+    // Работа с числами и операциями
     if (childElements.length === 0 || attributes.operation && element.nodeType === 1) {
         let text;
         if (!attributes.operation) {
             text = element.textContent.trim();
         }
-        console.log(attributes.operation)
         if (!isNaN(text)) {
             return text;
         } else if (constants[text]) {
             element.textContent = constants[text];
-            return parseValue(element, indent);
+            return parseValue(element, indent, constants);
         } else if (attributes.operation === "+" && childElements.length === 2) {
-            return `$[+ ${parseValue(childElements[0], indent)} ${parseValue(childElements[1], indent)}]`
+            return `$[+ ${parseValue(childElements[0], indent, constants)} ${parseValue(childElements[1], indent, constants)}]`
         } else if (attributes.operation === "-" && childElements.length === 2) {
-            return `$[- ${parseValue(childElements[0], indent)} ${parseValue(childElements[1], indent)}]`
+            return `$[- ${parseValue(childElements[0], indent, constants)} ${parseValue(childElements[1], indent, constants)}]`
         } else if (attributes.operation === "abs" && childElements.length === 1) {
-            return `$[abs ${parseValue(childElements[0], indent)}]`
+            return `$[abs ${parseValue(childElements[0], indent, constants)}]`
         } else if (attributes.operation === "min") {
-            return `$[min ${childElements.map(child => parseValue(child, indent)).join(" ")}]`
+            return `$[min ${childElements.map(child => parseValue(child, indent, constants)).join(" ")}]`
         }
     }
 
     // Проверка на массив
     if (childElements.length > 0 && childElements.every(child => child.tagName === childElements[0].tagName)) {
-        return `list(${childElements.map(child => parseValue(child, indent)).join(", ")})`;
+        return `list(${childElements.map(child => parseValue(child, indent, constants)).join(", ")})`;
     }
 
     // Проверка на словарь
@@ -51,7 +91,7 @@ export function parseValue(element, indent) {
 
         // Добавление элементов в словарь
         childElements.forEach(child => {
-            pairs.push(`    ${indent}${child.tagName} : ${parseValue(child, "    " + indent)}`);
+            pairs.push(`    ${indent}${child.tagName} : ${parseValue(child, "    " + indent, constants)}`);
         });
 
         return `{\n${pairs.join('\n')}\n${indent}}`;
@@ -99,37 +139,27 @@ function parseXMLToObject(node) {
         return parentObject;
     }
 
-    return null;
+    return;
 }
 
-const xmlString = `
-<r name="var" value="3" />
-<Book id="1" language="en">
-    <Title>1</Title>
-    <Tite>2</Tite>
-    <dict>
-        <c id="56">1</c>
-        <d operation="+">
-            <l>var</l>
-            <l operation="-">
-                <j>8</j>
-                <s>908</s>
-            </l>
-        </d>
-        <v>2</v>
-    </dict>
-</Book>
-`;
+function main() {
+    try {
+        const { xmlFilePath, txtFilePath } = readKeys();
+        const xmlString = readXmlFile(xmlFilePath);
+        const parser = new DOMParser();
+        const dom = parser.parseFromString(xmlString, "application/xml");
+        const rootNodes = Array.from(dom.childNodes)
+            .map(rootNode => parseXMLToObject(rootNode))
+            .filter(element => element !== "");
+        const constants = {};
+        const text = rootNodes
+            .map(elem => parseValue(elem, "", constants))
+            .join('\n'); 
 
-const parser = new DOMParser();
-const dom = parser.parseFromString(xmlString, "application/xml");
-const rootNodes = Array.from(dom.childNodes); // Все узлы на верхнем уровне
-const result = rootNodes
-    .map(rootNode => parseXMLToObject(rootNode))
-    .filter(item => item !== "");
-const constants = {};
-const resultString = result
-    .map(elem => parseValue(elem, "")) // Преобразуем каждый объект в строку
-    .join('\n'); 
-    
-console.log(resultString);
+        console.log(writeTxtFile(txtFilePath, text));
+    } catch(err) {
+        console.error(err.message);
+    }
+}
+
+main();
